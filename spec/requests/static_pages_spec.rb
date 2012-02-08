b973fe472039301bd451effc4fae6bdf04fa79f5
Alonso <alonso.aca@gmail.com>
2012-02-07 22:54:31 -0600

    Chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,11 +4,19 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { 'Home' }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('Home')) }
+    it_should_behave_like "all static pages"
+    
   end
 
   describe "Help page" do
@@ -21,8 +29,11 @@ describe "Static pages" do
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
+
   end
 
   describe "Contact page" do
@@ -31,4 +42,19 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('Home')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+  end
+  
 end
