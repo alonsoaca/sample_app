bd114fd51e54f8fbea7e108d4b9230dd51103ae1
Alonso <alonso.aca@gmail.com>
2012-02-19 01:13:09 -0600

    Finish user det, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:edit, :update, :index]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -16,5 +31,36 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private 
+  
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      logger.info { "Will check if the user: #{@user} is the same as #{current_user} " }
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
   
 end
