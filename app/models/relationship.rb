2289f98928ce60db5b35ea8cd512d0e57eba88f9
Alonso <alonso.aca@gmail.com>
2012-02-27 14:59:38 -0600

    half the way

---
+++ app/models/relationship.rb
@@ -0,0 +1,10 @@
+class Relationship < ActiveRecord::Base
+  attr_accessible :followed_id
+  
+  validates :followed_id, presence: true
+  validates :follower_id, presence: true
+  
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,15 @@ class User < ActiveRecord::Base
   
   has_many :microposts, dependent: :destroy
   
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+                                   
+  has_many :followers, through: :reverse_relationships, source: :follower
+  
   validates :name, presence: true, length: { maximum: 50 }
   
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -29,6 +38,18 @@ class User < ActiveRecord::Base
   def feed
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
   
   private
